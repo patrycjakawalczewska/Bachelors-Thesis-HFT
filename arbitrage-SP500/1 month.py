4bea5ac3a4dbb0f8e06dd02e57c72fff0932ee43
Patrycja Kawalczewska <90581454+patrycjakawalczewska@users.noreply.github.com>
2023-06-14 17:10:01 +0200

    Update and rename 1 month to 1 month.py

---
+++ arbitrage-SP500/1 month.py
@@ -0,0 +1,41 @@
+#import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+data = yf.download(['SPY', 'ES=F'], period='1ms', interval='1m', group_by='ticker')
+
+
+#specify the data
+PeriodStart = "2021-03-01"
+PeriodEnd  = "2021-03-31"
+tickerlist = ["ES=F", "SPY"]
+
+#download the data
+df = yf.download(tickerlist, start = PeriodStart, end = PeriodEnd)['Adj Close']
+df.head()
+
+
+#create a chart with different x axis, but the same y axis
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111)
+line1 = ax.plot(df['ES=F'], label = "ES=F", color = "green")
+ax2 = ax.twinx()
+line2 = ax2.plot(df['SPY'], label = "SPY", color = "blue")
+
+#add the second variable to the labels
+lines = line1 + line2
+labels = [1.get_label() for 1 in lines]
+lines = line1 + line2
+labels = [line.get_label() for line in lines]
+ax.legend(lines, labels, loc=0)
+
+#set the title and label axis
+plt.title('(a) 1 month')
+ax.grid()
+ax.set_xlabel('Time (CT)')
+ax.set_ylabel('Index Points (ES=F)')
+ax2.set_ylabel('Index Points (SPY)')
+
+#display the graph
+plt.show()
